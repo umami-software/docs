168f6c7af56a04f45f2956747933afdcb1431f61
Mike Cao <moocao@gmail.com>
2025-11-04 20:31:24 -0800

    v2 docs only.

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,47 +1,21 @@
-import { ReactNode } from 'react';
 import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
+import type { ReactNode } from 'react';
 import { baseOptions } from '@/lib/layout.shared';
 import { source } from '@/lib/source';
-import { Book, Map, Terminal, Cloud } from 'lucide-react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div id="docs-layout">
-      <DocsLayout
-        tree={source.pageTree}
-        sidebar={{
-          tabs: false,
-        }}
-        links={[
-          {
-            icon: <Book />,
-            text: 'Documentation',
-            url: '/docs',
-            active: 'url',
-          },
-          {
-            icon: <Map />,
-            text: 'Guides',
-            url: '/docs/guides',
-            active: 'nested-url',
-          },
-          {
-            icon: <Terminal />,
-            text: 'API Reference',
-            url: '/docs/api',
-            active: 'nested-url',
-          },
-          {
-            icon: <Cloud />,
-            text: 'Cloud',
-            url: '/docs/cloud',
-            active: 'nested-url',
-          },
-        ]}
-        {...baseOptions()}
-      >
-        {children}
-      </DocsLayout>
-    </div>
+    <DocsLayout
+      tree={source.pageTree}
+      sidebar={{
+        tabs: [{ title: 'v2', url: 'https://v2.umami.is/docs' }],
+      }}
+      themeSwitch={{
+        mode: 'light-dark',
+      }}
+      {...baseOptions()}
+    >
+      {children}
+    </DocsLayout>
   );
 }
